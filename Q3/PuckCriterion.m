85572cca0655967183d9c2b8d9f230c311ad221e
christophe <christophe@hatterer.net>
2023-04-01 16:28:10 +0200

    FIRST PLY FAILURE DONE

--- Q3/PuckCriterion.m
+++ Q3/PuckCriterion.m
@@ -2,6 +2,8 @@ function [FF,IFF] = PuckCriterion(sigma1,sigma2,sigma3,X_T,X_C,Y_T,Y_C,G12_t,nu1
 FF =0;
 IFF = 0;
 
+sigma3 = abs(sigma3);
+
 % Determination of which fiber used
 if fiber == 'c'
     m = 1.1;
@@ -22,22 +24,26 @@ elseif fiber == 'g'
 end
 
 R_TTA = Y_C/(2*(1+pcTT));
+tau_12c = G12_t*sqrt(1+2*pcTT);
 
 % Fiber failure (direction of the fibers)
 if sigma1 > 0
     FF = 1/X_T*(sigma1 - (nu12-nu12*m*E1/Ex)*(sigma2+sigma3));
     if print == 'y'
-        fprintf("fiber failure Tension\n")
+        fprintf("Evaluating Fiber Tension\n")
     end
 else 
     FF = 1/(-X_C)*(sigma1 - (nu12-nu12*m*E1/Ex)*(sigma2+sigma3));
     if print == 'y'
-        fprintf("Fibre failure Compression\n")
+        fprintf("Evaluating Compression\n")
     end
 end
 
 % Matrix Failure
-if sigma2 > 0  %&& Y_T > sigma2 %Condition Mode A
+if sigma2 > 0 
+    if sigma2 > Y_T || sigma3 > G12_t
+        IFF = max(abs(sigma2/Y_T),sigma3/G12_t);
+    elseif sigma2 < Y_T %Condition Mode A
     R_Tll = G12_t;
     R_Tt = Y_T;
     A_ = (1/R_Tt - ptTll/R_Tll)*sigma2;
@@ -47,7 +53,10 @@ if sigma2 > 0  %&& Y_T > sigma2 %Condition Mode A
     if print == 'y'
         fprintf("Interfiber failure Mode A\n")
     end
-elseif sigma2 < 0 && -R_TTA < sigma2  %  Condition Mode B
+    end
+elseif sigma2 < -Y_C || sigma3 > tau_12c
+    IFF =max(abs(sigma2/Y_C),sigma3/tau_12c);
+elseif sigma2 <= 0 && -R_TTA < sigma2  %  Condition Mode B
     R_Tll = G12_t;
     A_ = (pcTll./R_Tll).*sigma2;
     B = sigma3./R_Tll;
@@ -56,7 +65,7 @@ elseif sigma2 < 0 && -R_TTA < sigma2  %  Condition Mode B
     if print == 'y'
         fprintf("Interfiber failure Mode B\n")
     end
-elseif sigma2 < -R_TTA %&& -Y_C <= sigma2 % Condition Mode C
+elseif sigma2 < -R_TTA && -Y_C <= sigma2 % Condition Mode C
     R_Tll = Y_C;
     A_ = sigma3./(2*(1+pcTT)*R_Tll);
     B = sigma2./R_Tll;
@@ -72,4 +81,5 @@ else
         fprintf("invalid value of sigma2\n")
     end
 end
+
 
